f93472a413c4651685ae77573e96c2320169f8fe
Joe-cyber <caliendo.gioacchino00@gmail.com>
2022-04-07 11:55:16 +0200

    second test commit git

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 if __name__ == '__main__':
     print("welcome into my project!")
-    print("welcome into my project!")
-    print("welcome into my project!")
-    print("welcome into my project!")
-    print("welcome into my project!")
