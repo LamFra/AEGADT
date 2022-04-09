615dd2f3dcde54faf731a823944efcc8e5fa21c5
Francesca La Manna <francescalm87@gmail.com>
2022-04-09 17:10:30 +0200

    creating function to load dataset

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import numpy as np
+import csv
+
+
+FILE_NAME = "dataset/adult_dataset.data"
+
+
+def load_dataset():
+    with open(FILE_NAME, "r") as f:
+        dataset = list(csv.reader(f, delimiter=','))
+
+    return np.array(dataset, dtype=object)
+
+
 if __name__ == '__main__':
-    print("welcome into my project!")
+    data = load_dataset()
+    print(data)
