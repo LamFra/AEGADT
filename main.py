f7c8328b81717956a6226cf231530660fdd88cd8
Francesca La Manna <francescalm87@gmail.com>
2022-04-09 18:06:11 +0200

    chancing dataset and updating function to load it

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import numpy as np
 import csv
+from sklearn import tree
 
-
-FILE_NAME = "dataset/adult_dataset.data"
+FILE_NAME = "dataset/Skin_NonSkin.txt"
 
 
 def load_dataset():
     with open(FILE_NAME, "r") as f:
-        dataset = list(csv.reader(f, delimiter=','))
-
-    return np.array(dataset, dtype=object)
+        dataset = list(csv.reader(f, delimiter='\t'))
+    data = np.array(dataset, dtype=object)
+    return np.hsplit(data, [3, 4])[0], np.hsplit(data, [3, 4])[1]
 
 
 if __name__ == '__main__':
-    data = load_dataset()
-    print(data)
+    X, y = load_dataset()
+    print(X)
+    print(y)
+    """
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X, y)
+    tree.plot_tree(clf)
+    tree.export_graphviz(clf, out_file='tree.dot')
+    """
