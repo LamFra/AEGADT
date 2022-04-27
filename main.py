73c7bc0170af412cbe825866dcad4313f5e908a5
Francesca La Manna <francescalm87@gmail.com>
2022-04-27 16:58:15 +0200

    integrating metrics

--- main.py
+++ main.py
@@ -1,117 +1,105 @@
-import pickle
-import random
-
 import numpy as np
 import pandas as pd
-from sklearn import tree
-from sklearn.metrics import precision_score, recall_score, f1_score
+import random
+
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import VotingClassifier
+from sklearn import tree
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# from sklearn.datasets import load_breast_cancer
 
 TRAIN_FILE_NAME = "dataset/winequality-white.csv"
 TEST_FILE_NAME = "dataset/winequality-white_test.csv"
-POPULATION = 50
-GENERATION = 3
+
+
+# defining various steps required for the genetic algorithm
+def initilization_of_population(size, n_feat):
+    population = []
+    for i in range(size):
+        chromosome = np.ones(n_feat, dtype=bool)
+        chromosome[:int(0.3 * n_feat)] = False
+        np.random.shuffle(chromosome)
+        population.append(chromosome)
+    return population
+
+
+def fitness_score(population):
+    scores = []
+    for chromosome in population:
+        logmodel.fit(X_train.iloc[:, chromosome], y_train)
+        predictions = logmodel.predict(X_test.iloc[:, chromosome])
+        scores.append(precision_score(y_test, predictions, average='micro') * 0.4 + recall_score(y_test, predictions, average='micro') * 0.3 + f1_score(y_test, predictions, average='micro') * 0.3)
+    scores, population = np.array(scores), np.array(population)
+    inds = np.argsort(scores)
+    return list(scores[inds][::-1]), list(population[inds, :][::-1])
+
+
+def selection(pop_after_fit, n_parents):
+    population_nextgen = []
+    for i in range(n_parents):
+        population_nextgen.append(pop_after_fit[i])
+    return population_nextgen
+
+
+def crossover(pop_after_sel):
+    population_nextgen = pop_after_sel
+    for i in range(len(pop_after_sel)):
+        child = pop_after_sel[i]
+        child[3:7] = pop_after_sel[(i + 1) % len(pop_after_sel)][3:7]
+        population_nextgen.append(child)
+    return population_nextgen
+
+
+def mutation(pop_after_cross, mutation_rate):
+    population_nextgen = []
+    for i in range(0, len(pop_after_cross)):
+        chromosome = pop_after_cross[i]
+        for j in range(len(chromosome)):
+            if random.random() < mutation_rate:
+                chromosome[j] = not chromosome[j]
+        population_nextgen.append(chromosome)
+    # print(population_nextgen)
+    return population_nextgen
+
+
+def generations(size, n_feat, n_parents, mutation_rate, n_gen, X_train, X_test, y_train, y_test):
+    best_chromo = []
+    best_score = []
+    population_nextgen = initilization_of_population(size, n_feat)
+    for i in range(n_gen):
+        scores, pop_after_fit = fitness_score(population_nextgen)
+        # print(scores[:2])
+        pop_after_sel = selection(pop_after_fit, n_parents)
+        pop_after_cross = crossover(pop_after_sel)
+        population_nextgen = mutation(pop_after_cross, mutation_rate)
+        best_chromo.append(pop_after_fit[0])
+        best_score.append(scores[0])
+    return best_chromo, best_score
 
 
 def split_dataset(filename):
     with open(filename, "r"):
         dataset = pd.read_csv(filename, header=0, sep=';')
-    data = np.array(dataset, dtype=int)
-    x, y = np.hsplit(data, [11, 12])[0], np.hsplit(data, [11, 12])[1]
-    return train_test_split(x, y)
+    features = np.genfromtxt(filename, delimiter=';', dtype=str, max_rows=1)
+    features = features[:-1]
+    wine = np.array(dataset, dtype=float)
+    data, label = np.hsplit(wine, [11, 12])[0], np.hsplit(wine, [11, 12])[1]
+    df = pd.DataFrame(data, columns=features)
+    return df, label
 
 
-def split_train_set(filename):
-    with open(filename, "r"):
-        dataset = pd.read_csv(filename, header=0, sep=';')
-    data = np.array(dataset, dtype=int)
-    return np.hsplit(data, [11, 12])[0], np.hsplit(data, [11, 12])[1]
+if __name__ == "__main__":
+    # splitting the model into training and testing set
+    df, label = split_dataset(TRAIN_FILE_NAME)
+    # splitting the model into training and testing set
+    X_train, X_test, y_train, y_test = train_test_split(df, label, test_size=0.30, random_state=101)
+    logmodel = tree.DecisionTreeClassifier(max_depth=4)
+    logmodel.fit(X_train, y_train)
+    predictions = logmodel.predict(X_test)
+    print("Metric = " + str(accuracy_score(y_test, predictions)))
 
-
-def save_decision_tree(mod):
-    tree.plot_tree(mod)
-    tree.export_graphviz(mod, out_file='tree.dot')
-
-
-def generate_initial_population(population):
-    def generate_tree():
-        x_tr, x_te, y_tr, y_te = split_dataset(TRAIN_FILE_NAME)
-        clf = tree.DecisionTreeClassifier(max_depth=4)
-        return clf.fit(x_tr, y_tr)
-
-    mod = []
-    for _ in range(population):
-        mod.append(generate_tree())
-    return mod
-
-
-def score_single_tree():
-    x_tr, x_te, y_tr, y_te = split_dataset(TRAIN_FILE_NAME)
-    model_name = 'models/decisionTree1.sav'
-    try:
-        mod = pickle.load(open(model_name, 'rb'))
-    except FileNotFoundError:
-        clf = tree.DecisionTreeClassifier()
-        mod = clf.fit(x_tr, y_tr)
-        pickle.dump(mod, open(model_name, 'wb'))
-    result = mod.score(x_te, y_te)
-    return result
-
-
-def selection(mod):
-    x_test, y_test = split_train_set(TEST_FILE_NAME)
-    a = []
-    for m in mod:
-        pred = m.predict(x_test)
-        a.append(precision_score(y_test, pred, average='micro') * 0.4 + recall_score(y_test, pred, average='micro') * 0.3 + f1_score(y_test, pred, average='micro') * 0.3)
-    d = []
-    for i, j in zip(a, mod):
-        d.append((i, j))
-    d.sort(key=lambda tup: tup[0], reverse=True)
-    return d
-
-
-def next_gen(actual_gen):
-    def generate_child(m):
-        ch = VotingClassifier(m, voting="hard")
-        x_tr, x_te, y_tr, y_te = split_dataset(TRAIN_FILE_NAME)
-        return ch.fit(x_tr, y_tr.ravel())
-
-    def random_combination(iterable, r):
-        pool = tuple(iterable)
-        n = len(pool)
-        indices = sorted(random.sample(range(n), r))
-        a = []
-        for j in indices:
-            a.append(pool[j])
-        return a
-
-    mod = []
-    for c, i in zip(actual_gen, range(len(actual_gen))):
-        mod.append(("clf" + str(i), c[1]))
-
-    new_gen = []
-    for _ in range(POPULATION):
-        new_gen.append(generate_child(random_combination(mod, 3)))
-
-    return new_gen
-
-
-if __name__ == '__main__':
-    s = 0.3
-    models = generate_initial_population(POPULATION)
-    generation = selection(models)
-    save_decision_tree(generation[0][1])
-    for i in range(GENERATION-1):
-        generation = generation[:int(len(generation) * s)]
-        print("Accuracy generation %d: %s" % (i+1, generation[0][0]))
-        ng = next_gen(generation)
-        array = []
-        for n in ng:
-            x_tr, x_te, y_tr, y_te = split_dataset(TRAIN_FILE_NAME)
-            array.append(n.fit(x_tr, y_tr.ravel()))
-        newgen = selection(array)
-        generation = newgen
-
-    print("Accuracy generation %d: %s" % (GENERATION, generation[0][0]))
+    chromo, score = generations(size=10, n_feat=11, n_parents=5, mutation_rate=0.10, n_gen=10, X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
+    logmodel.fit(X_train.iloc[:, chromo[-1]], y_train)
+    predictions = logmodel.predict(X_test.iloc[:, chromo[-1]])
+    print("Metric score after genetic algorithm is= " + str(accuracy_score(y_test, predictions)))
